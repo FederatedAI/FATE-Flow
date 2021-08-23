e488338a135213df44c4aa3a99cc2df33566d3df
weiwee <wbwmat@gmail.com>
2021-08-23 15:10:42 +0800

    fix cache
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- python/fate_flow/operation/task_executor.py
+++ python/fate_flow/operation/task_executor.py
@@ -54,6 +54,7 @@ class ComponentInput:
         job_parameters,
         roles,
         flow_feeded_parameters,
+        caches,
     ) -> None:
         self._tracker = tracker
         self._checkpoint_manager = checkpoint_manager
@@ -65,6 +66,7 @@ class ComponentInput:
         self._job_parameters = job_parameters
         self._roles = roles
         self._flow_feeded_parameters = flow_feeded_parameters
+        self._caches = caches
 
     @property
     def tracker(self):
