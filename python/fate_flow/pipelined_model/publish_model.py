994bf1e532979c5c6d490d64c8d95424208fccf0
Jat <jat@sinosky.org>
2021-12-16 23:51:01 +0800

    convert party id to string on grpc

--- python/fate_flow/pipelined_model/publish_model.py
+++ python/fate_flow/pipelined_model/publish_model.py
@@ -80,32 +80,36 @@ def load_model(config_data):
 
 
 def bind_model_service(config_data):
-    service_id = config_data.get('service_id')
-    initiator_role = config_data['initiator']['role']
-    initiator_party_id = config_data['initiator']['party_id']
-    model_id = config_data['job_parameters']['model_id']
-    model_version = config_data['job_parameters']['model_version']
     if not config_data.get('servings'):
         return 100, 'Please configure servings address'
-    for serving in config_data.get('servings'):
+
+    service_id = str(config_data.get('service_id', ''))
+    initiator_role = config_data['initiator']['role']
+    initiator_party_id = str(config_data['initiator']['party_id'])
+    model_id = config_data['job_parameters']['model_id']
+    model_version = config_data['job_parameters']['model_version']
+
+    for serving in config_data['servings']:
         with grpc.insecure_channel(serving) as channel:
             stub = model_service_pb2_grpc.ModelServiceStub(channel)
             publish_model_request = model_service_pb2.PublishRequest()
             publish_model_request.serviceId = service_id
             for role_name, role_party in config_data.get("role").items():
-                publish_model_request.role[role_name].partyId.extend(role_party)
+                publish_model_request.role[role_name].partyId.extend(str(role_party))
 
+            party_model_id = model_utils.gen_party_model_id(model_id, initiator_role, initiator_party_id)
             publish_model_request.model[initiator_role].roleModelInfo[initiator_party_id].tableName = model_version
-            publish_model_request.model[initiator_role].roleModelInfo[
-                initiator_party_id].namespace = model_utils.gen_party_model_id(model_id, initiator_role,
-                                                                               initiator_party_id)
+            publish_model_request.model[initiator_role].roleModelInfo[initiator_party_id].namespace = party_model_id
+
             publish_model_request.local.role = initiator_role
             publish_model_request.local.partyId = initiator_party_id
+
             stat_logger.info(publish_model_request)
             response = stub.publishBind(publish_model_request)
             stat_logger.info(response)
             if response.statusCode != 0:
                 return response.statusCode, response.message
+
     return 0, None
 
 
