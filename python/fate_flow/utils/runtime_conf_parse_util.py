dfcdeb1e88bb20994726f1b4dd4f6709ba1f9b8e
zhihuiwan <15779896112@163.com>
2022-08-05 20:03:41 +0800

    fix provider bug
    Signed-off-by: zhihuiwan <15779896112@163.com>

--- python/fate_flow/utils/runtime_conf_parse_util.py
+++ python/fate_flow/utils/runtime_conf_parse_util.py
@@ -246,7 +246,9 @@ class RuntimeConfParserUtil(object):
             return provider_info
         else:
             if local_party_id is None or local_role is None \
-                    or local_role not in submit_dict["role"] or local_party_id not in submit_dict["role"][local_role]:
+                    or local_role not in submit_dict["role"] or \
+                    (str(local_party_id) not in submit_dict["role"][local_role]
+                     and int(local_party_id) not in submit_dict["role"][local_role]):
                 raise ValueError("when parse provider from conf, local role & party_id should should be None")
 
             provider_info_all_party = {}
