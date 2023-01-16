24ce9e5ce03241d005182910dea632628b8dbb56
zhihuiwan <15779896112@163.com>
2023-01-16 15:18:22 +0800

    fateflow: update env
    Signed-off-by: zhihuiwan <15779896112@163.com>

---
+++ bin/init_env.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#
+#  Copyright 2019 The FATE Authors. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+fate_project_base=$(cd `dirname "$(realpath "${BASH_SOURCE[0]:-${(%):-%x}}")"`; cd ../;pwd)
+export FATE_PROJECT_BASE=$fate_project_base
+export FATE_DEPLOY_BASE=$fate_project_base
+export EGGROLL_HOME=
+export PYTHONPATH=
+exprot FATE_ENV=
+
+export FATE_LOG_LEVEL=DEBUG
+
+venv=
+export JAVA_HOME=
+export PATH=$PATH:$JAVA_HOME/bin
+source ${venv}/bin/activate
+
