af15c23c15bc09580ab9f90c4af19f16e34b059f
zhihuiwan <15779896112@163.com>
2023-04-27 13:58:23 +0800

    add log manager
    Signed-off-by: zhihuiwan <15779896112@163.com>

--- python/fate_flow/apps/client/log_app.py
+++ python/fate_flow/apps/client/log_app.py
@@ -15,15 +15,29 @@
 #
 from webargs import fields
 
+from fate_flow.manager.log.log_manager import LogManager
 from fate_flow.utils.api_utils import API
 
 
 @manager.route('/count', methods=['GET'])
 @API.Input.json(log_type=fields.String(required=True))
 @API.Input.json(job_id=fields.String(required=True))
-@API.Input.json(role=fields.String(required=True))
-@API.Input.json(party_id=fields.String(required=True))
-@API.Input.json(task_name=fields.String(required=True))
-def log_count(log_type, job_id, role, party_id, task_name):
-    data = LogCollector(**request_data).count()
-    return API.Output.json(data={"count": 0})
+@API.Input.json(role=fields.String(required=False))
+@API.Input.json(party_id=fields.String(required=False))
+@API.Input.json(task_name=fields.String(required=False))
+def count(log_type, job_id, role=None, party_id=None, task_name=None):
+    data = LogManager(log_type, job_id, role=role, party_id=party_id, task_name=task_name).count()
+    return API.Output.json(data=data)
+
+
+@manager.route('/query', methods=['GET'])
+@API.Input.json(log_type=fields.String(required=True))
+@API.Input.json(job_id=fields.String(required=True))
+@API.Input.json(role=fields.String(required=False))
+@API.Input.json(party_id=fields.String(required=False))
+@API.Input.json(task_name=fields.String(required=False))
+@API.Input.json(begin=fields.Integer(required=False))
+@API.Input.json(end=fields.Integer(required=False))
+def get(log_type, job_id, begin, end, role=None, party_id=None, task_name=None):
+    data = LogManager(log_type, job_id, role=role, party_id=party_id, task_name=task_name).cat_log(begin=begin, end=end)
+    return API.Output.json(data=data)
