f0aab770b10d085b7445ddb43e4fd1103f6c6334
zhihuiwan <15779896112@163.com>
2023-05-16 18:31:59 +0800

    fateflow: fix provider bug
    Signed-off-by: zhihuiwan <15779896112@163.com>

--- python/fate_flow/utils/process_utils.py
+++ python/fate_flow/utils/process_utils.py
@@ -59,7 +59,7 @@ def run_subprocess(job_id, config_dir, process_cmd, added_env: dict = None, log_
                 value += ':' + subprocess_env[name]
             subprocess_env[name] = value
     subprocess_env.pop("CLASSPATH", None)
-
+    logger.info(f"RUN ENV: {subprocess_env}")
     p = subprocess.Popen(process_cmd,
                          stdout=std,
                          stderr=std,
