0bf5568ba7576784186dab5ab389fbde630b7932
zhihuiwan <15779896112@163.com>
2023-07-11 20:47:52 +0800

    add component download
    Signed-off-by: zhihuiwan <15779896112@163.com>

--- python/fate_flow/components/components/__init__.py
+++ python/fate_flow/components/components/__init__.py
@@ -13,5 +13,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 from .upload import upload
+from .download import download
 
-BUILDIN_COMPONENTS = [upload]
+BUILDIN_COMPONENTS = [upload, download]
