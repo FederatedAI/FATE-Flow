c53839e60a030c95f61e6629d61eb7207d715ae5
zhihuiwan <15779896112@163.com>
2023-08-24 21:06:31 +0800

    init setup
    Signed-off-by: zhihuiwan <15779896112@163.com>

--- python/fate_flow/hook/common/__init__.py
+++ python/fate_flow/hook/common/__init__.py
@@ -1,3 +1,4 @@
+
 #
 #  Copyright 2019 The FATE Authors. All Rights Reserved.
 #
@@ -12,27 +13,3 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-#
-# GRPC
-GRPC_SERVER_MAX_WORKERS = None  # default: (os.cpu_count() or 1) * 5
-
-# Request
-HTTP_REQUEST_TIMEOUT = 10  # s
-REMOTE_REQUEST_TIMEOUT = 30000  # ms
-
-LOG_LEVEL = 20
-LOG_DIR = ""
-DATA_DIR = ""
-MODEL_DIR = ""
-JOB_DIR = ""
-DEFAULT_FATE_DIR = ""
-
-# sqlite
-SQLITE_FILE_DIR = ""
-SQLITE_FILE_NAME = "fate_flow_sqlite.db"
-
-
-# Client Manager
-APP_TOKEN_LENGTH = 16
-ADMIN_ID = "admin"
-ADMIN_KEY = "fate_flow_admin"
--- python/fate_flow/runtime/env.py
+++ python/fate_flow/runtime/env.py
@@ -5,7 +5,6 @@ import fate_flow
 def is_in_virtualenv():
     try:
         module_path = fate_flow.__file__
-        return True
         return sys.prefix in module_path
     except ImportError:
         return False
