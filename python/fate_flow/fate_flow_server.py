e7ec9e3311a7210c49d5903ba9db8823c78c68cb
zhihuiwan <15779896112@163.com>
2023-09-12 17:25:33 +0800

    update adapter db
    Signed-off-by: zhihuiwan <15779896112@163.com>

--- python/fate_flow/fate_flow_server.py
+++ python/fate_flow/fate_flow_server.py
@@ -20,6 +20,8 @@ import traceback
 
 import grpc
 from werkzeug.serving import run_simple
+
+from fate_flow.adapter import init_adapter
 from fate_flow.apps import app
 from fate_flow.controller.config_manager import ConfigManager
 from fate_flow.hook import HookManager
@@ -56,6 +58,8 @@ def server_init():
 
     # set signal
     signal.signal(signal.SIGCHLD, process_utils.wait_child_process)
+
+    init_adapter()
 
     # init db
     init_flow_db()
