cecee3e6bb3eb5534ed05ecc70b0646f3f3b9af9
zhihuiwan <15779896112@163.com>
2023-10-23 14:49:40 +0800

    Code adjustment
    Signed-off-by: zhihuiwan <15779896112@163.com>

--- python/fate_flow/manager/outputs/model/__init__.py
+++ python/fate_flow/manager/outputs/model/__init__.py
@@ -12,3 +12,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+from fate_flow.manager.outputs.model.model_manager import PipelinedModel
+from fate_flow.manager.outputs.model.model_meta import ModelMeta
+
+__all__ = ["PipelinedModel", "ModelMeta"]
