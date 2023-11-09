f5d3decbbd026b5ef10a41ed6e0e06a16834b5fa
zhihuiwan <15779896112@163.com>
2023-11-09 15:22:08 +0800

    fate container
    Signed-off-by: zhihuiwan <15779896112@163.com>

--- python/fate_flow/adapter/bfia/container/entrypoint/runner.py
+++ python/fate_flow/adapter/bfia/container/entrypoint/runner.py
@@ -12,15 +12,18 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-from .resource import BaseAPI
+#
+
+class Submit:
+    @staticmethod
+    def run():
+        import click
+        from fate_flow.adapter.bfia.container.entrypoint.cli import component
+
+        cli = click.Group()
+        cli.add_command(component)
+        cli(prog_name="python -m fate_flow.adapter.bfia.container.entrypoint")
 
 
-class Worker(BaseAPI):
-    def report_task_status(self, status, task_id, role):
-        return self.client.post(
-            endpoint="/v1/platform/schedule/task/callback",
-            json={
-                "status": status,
-                "task_id": task_id,
-                "role": role
-            })
+if __name__ == "__main__":
+    Submit.run()
--- python/fate_flow/manager/service/worker_manager.py
+++ python/fate_flow/manager/service/worker_manager.py
@@ -33,17 +33,19 @@ from fate_flow.utils.log_utils import failed_log, schedule_logger, start_log, su
 class WorkerManager:
     @classmethod
     def start_task_worker(cls, worker_name, task_info, task_parameters=None, executable=None, common_cmd=None,
-                          extra_env: dict = None, record=False, stderr=None, sync=False, **kwargs):
+                          extra_env: dict = None, record=False, stderr=None, sync=False, config_dir=None, std_dir=None,
+                          **kwargs):
         if not extra_env:
             extra_env = {}
         worker_id = uuid1().hex
-        config_dir, std_dir = cls.get_process_dirs(
-            job_id=task_info.get("job_id"),
-            role=task_info.get("role"),
-            party_id=task_info.get("party_id"),
-            task_name=task_info.get("task_name"),
-            task_version=task_info.get("task_version")
-        )
+        if not config_dir or not std_dir:
+            config_dir, std_dir = cls.get_process_dirs(
+                job_id=task_info.get("job_id"),
+                role=task_info.get("role"),
+                party_id=task_info.get("party_id"),
+                task_name=task_info.get("task_name"),
+                task_version=task_info.get("task_version")
+            )
         params_env = {}
         if task_parameters:
             params_env = cls.get_env(task_info.get("job_id"), task_parameters)
