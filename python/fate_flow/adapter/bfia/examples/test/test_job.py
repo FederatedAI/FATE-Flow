f62e1eea93eca929a31d3d29569f743370d2aaf6
zhihuiwan <15779896112@163.com>
2023-12-04 11:27:43 +0800

    update bfia example
    Signed-off-by: zhihuiwan <15779896112@163.com>

--- python/fate_flow/adapter/bfia/examples/test/test_job.py
+++ python/fate_flow/adapter/bfia/examples/test/test_job.py
@@ -7,18 +7,18 @@ base_url = "http://127.0.0.1:9380"
 
 def register_components():
     uri = "/v2/provider/register"
-    config_path = "../job/fate/fate_components.json"
+    config_path = "../job/unionpay/unionpay_components.json"
     body = json.load(open(config_path, "r"))
     resp = requests.post(base_url+uri, json=body)
     print(resp.text)
 
 
-register_components()
+# register_components()
 
 
 def submit_job():
     uri = "/v1/platform/schedule/job/create_all"
-    config_path = "../job/fate/fate_psi_lr.json"
+    config_path = "../job/unionpay/bfia_psi_sbt.json"
     body = json.load(open(config_path, "r"))
     resp = requests.post(base_url+uri, json=body)
     print(resp.text)
