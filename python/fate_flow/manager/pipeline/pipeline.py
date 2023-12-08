68b8c991e363cb80e8534b0125586f189b33233b
chengtcc <864261919@qq.com>
2023-12-08 18:08:22 +0800

    add log&dag&upload
    Signed-off-by: chengtcc <864261919@qq.com>

--- python/fate_flow/manager/pipeline/pipeline.py
+++ python/fate_flow/manager/pipeline/pipeline.py
@@ -1,81 +1,83 @@
 from fate_flow.manager.operation.job_saver import JobSaver
+from fate_flow.hub.flow_hub import FlowHub
+from fate_flow.entity.spec.dag import DAGSchema
+from fate_flow.entity.spec.dag import DataWarehouseChannelSpec
+from fate_flow.controller.parser import JobParser
 
 
 def pipeline_dag_dependency(job):
     component_list = []
-    component_module, dependence_dict, component_need_run = {}, {}, {}
-
+    component_module, dependence_dict, component_need_run, component_stage = {}, {}, {}, {}
+    # job_parser = FlowHub.load_job_parser(DAGSchema(**job.f_dag))
+    job_parser = JobParser(DAGSchema(**job.f_dag))
     tasks = job.f_dag["dag"].get("tasks")
     for name, components in tasks.items():
         component_list.append(name)
         component_module[name] = components["component_ref"]
         dependence_dict[name] = []
 
-    for name, components in tasks.items():
-        dependence_tasks = components["dependent_tasks"]
-        inputs = components.get("inputs", None)
-        if 'data' in inputs:
-            data_input = inputs["data"]
-            for data_key, data_dct in data_input.items():
-                for _k, dataset in data_dct.items():
-                    if isinstance(dataset, list):
-                        dataset = dataset[0]
-                    up_component_name = dataset.get("producer_task")
-                    # up_pos = component_list.index(up_component_name)
-                    # up_component = components[up_pos]
-                    # data_name = dataset.split(".", -1)[1]
-                    # if up_component.get_output().get("data"):
-                    #     data_pos = up_component.get_output().get("data").index(data_name)
-                    # else:
-                    #     data_pos = 0
-
-                    if data_key == "data" or data_key == "train_data":
-                        data_type = data_key
-                    else:
-                        data_type = "validate_data"
-
-                    dependence_dict[name].append({"component_name": up_component_name,
-                                                  "type": data_type,
-                                                  "up_output_info": [data_type, 0]})
-
-        input_keyword_type_mapping = {"model": "model",
-                                      "isometric_model": "model",
-                                      "cache": "cache"}
-        for keyword, v_type in input_keyword_type_mapping.items():
-            if keyword in inputs:
-                input_list = inputs[keyword]
-                if not input_list or not isinstance(input_list, dict):
-                    continue
-                # if isinstance(input_list, list):
-                #     input_list = input_list[0]
-                for _k, _input in input_list.items():
-                    if isinstance(_input, list):
-                        _input = _input[0]
-                    up_component_name = _input.get("producer_task")
-                    if up_component_name == "pipeline":
-                        continue
-                    # link_alias = _input.split(".", -1)[1]
-                    # up_pos = component_list.index(up_component_name)
-                    # up_component = self.components[up_pos]
-                    # if up_component.get_output().get(v_type):
-                    #     dep_pos = up_component.get_output().get(v_type).index(link_alias)
-                    # else:
-                    dep_pos = 0
-                    dependence_dict[name].append({"component_name": up_component_name,
-                                                  "type": v_type,
-                                                  "up_output_info": [v_type, dep_pos]})
-
-        if not dependence_dict[name]:
-            del dependence_dict[name]
-
     tasks = JobSaver.query_task(job_id=job.f_job_id, party_id=job.f_party_id, role=job.f_role, only_latest=True)
     for task in tasks:
         need_run = task.f_component_parameters.get("ComponentParam", {}).get("need_run", True)
         component_need_run[task.f_task_name] = need_run
 
+        task_node = job_parser.get_task_node(task_name=task.f_task_name)
+        component_stage[task.f_task_name] = task_node.stage
+        upstream_inputs = task_node.upstream_inputs[job.f_role][job.f_party_id]
+        model_type_list = list(upstream_inputs.keys())
+
+        for model_type in model_type_list:
+            for data_type in list(upstream_inputs[model_type].keys()):
+                data_value = upstream_inputs[model_type][data_type]
+                if isinstance(data_value, list):
+                    for value in data_value:
+                        data_source = False
+                        if isinstance(value, DataWarehouseChannelSpec):
+                            up_output_info = []
+                            data_source = True
+                        else:
+                            up_output_info = [value.output_artifact_key]
+                        if task.f_task_name == value.producer_task:
+                            continue
+                        dependence_dict[task.f_task_name].append({
+                            "type": data_type,
+                            "model_type": model_type,
+                            "data_source": data_source,
+                            "component_name": value.producer_task if up_output_info else None,
+                            "up_output_info": up_output_info,
+                            "name": value.name if not up_output_info else None,
+                            "name_space": value.name if not up_output_info else None,
+                        })
+                else:
+                    data_source = False
+                    if isinstance(data_value, DataWarehouseChannelSpec):
+                        up_output_info = []
+                        data_source = True
+                    else:
+                        up_output_info = [data_value.output_artifact_key]
+
+                    if task.f_task_name == data_value.producer_task:
+                        continue
+                    dependence_dict[task.f_task_name].append({
+                        "type": data_type,
+                        "model_type": model_type,
+                        "data_source": data_source,
+                        "component_name": data_value.producer_task if up_output_info else None,
+                        "up_output_info": up_output_info,
+                        "name": data_value.name if not up_output_info else None,
+                        "name_space": data_value.namespace if not up_output_info else None,
+                    })
+
+        if not model_type_list:
+            dependence_dict[task.f_task_name].append({
+                "data_source": True,
+                "up_output_info": []})
+
     base_dependency = {"component_list": component_list,
-                       "dependencies": dependence_dict,
+                       "component_stage": component_stage,
                        "component_module": component_module,
-                       "component_need_run": component_need_run }
+                       "component_need_run": component_need_run,
+                       "dependencies": dependence_dict
+                       }
 
     return base_dependency
